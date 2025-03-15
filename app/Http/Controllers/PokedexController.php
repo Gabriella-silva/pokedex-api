53ac4f15c4b9626b14e58928b9c6dc25ee0f9091
gabriella-silva <gabriellaferreira534@gmail.com>
2025-03-15 16:03:04 -0300

    <3

--- app/Http/Controllers/PokedexController.php
+++ app/Http/Controllers/PokedexController.php
@@ -4,23 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\pokedex;
 use Illuminate\Http\Request;
+use Ilumminate\Support\Facades\Validator;
+use App\Http\Controllers\Response;
+/**Response cria um tratamento de exceções ou responde em HTML e Json e Direcionamentos e validator valida dados da API */
 
 class PokedexController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
+     /**READ! */
     public function index()
     {
-        //
+        $pokedex = pokedex::ALL();
+
+    $contador = $nome->count();
+    return ("Existem $contador pokemons na pokedex");
     }
 
     /**
      * Store a newly created resource in storage.
      */
+    /**CREAT */
     public function store(Request $request)
     {
-        //
+        $nome = $request->ALL();
+
+        $validacao = Validator::make($nome,[
+            'nome' => 'required',
+            'tipo' => 'required',
+            'pokefacts' => 'required',
+        ]);
+
+        if (validacao->fails()){
+            return 'Deu Pau na validação: '.Response()->json([Response::HTTP_NO_CONTENT]);
+        };
+
+        $pokedex = pokedex::create($nome);
+if($pokedex){
+    return 'registros cadastrados:' .Response()->json([Response::HTTP_CREATED]);}  
+    else{
+        return 'Registros não cadastrados: '.Response()->json([],Response::HTTP_NO_CONTENT);
     }
 
     /**
